47ffb3c65fe9176b61f1bf0fb27a6b6209103701
MandarDani\Mandar <mandardani@gmail.com>
2017-12-03 16:29:32 -0500

    Publishing 'Hello World' program and its documentation

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * THis is Hello world class
+ */
+public class HelloWorld
+{
+	/**
+	 * this is main method.
+	 */
+	public static void main(String [] args){
+	System.out.println("Hello!!!");
+	}
+}
